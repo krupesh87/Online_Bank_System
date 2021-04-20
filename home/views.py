37b1b1b981adc29887e8788be614f5daffba0e07
krupesh87 <krupeshpatel.64.d.fe@gmail.com>
2021-04-20 17:30:30 +0530

    second commit

--- home/views.py
+++ home/views.py
@@ -89,7 +89,6 @@ def transfer(request):
 
 
 
-
 def transfer_money(request):
     transfer = Transfer_money.objects.all().order_by('id').reverse()
     return render(request, 'transfer_hist.html', {'transfer': transfer})
